fff70a17cb6059c16d690a59b146fa9959dbfb96
Yarne Gooris <yarne.gooris@gmail.com>
2023-04-14 15:53:08 +0200

    Added More Analyses

--- js/analyse.js
+++ js/analyse.js
@@ -7,7 +7,6 @@ const firebaseConfig = {
     apiKey: "AIzaSyA4R3_Qmo2k4LyMtXs86xTkHtx9tIM8VoA",
     authDomain: "hoeve-lootens-497f9.firebaseapp.com",
     projectId: "hoeve-lootens-497f9",
-    storageBucket: "hoeve-lootens-497f9.appspot.com",
     messagingSenderId: "175696391830",
     appId: "1:175696391830:web:6e836280bf7b23259a1cb7",
     measurementId: "G-6LXS0WL2CT"
