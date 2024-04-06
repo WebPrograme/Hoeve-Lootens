eb2cc8c077d6996ef2f46571cf2ad3dd4369e89c
Yarne Gooris <yarne.gooris@gmail.com>
2024-04-06 16:01:08 +0200

    Update main.js with dynamic page titles and mobile navigation changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
+if (document.querySelector('.nav-list-active')) {
+	document.title = 'Hoeve Lootens - ' + document.querySelector('.nav-list-active').innerHTML;
+} else if (window.location.pathname === '/pages/shop.html') {
+	document.title = 'Hoeve Lootens - Shop';
+} else if (window.location.pathname === '/pages/volunteers.html') {
+	document.title = 'Hoeve Lootens - Vrijwilligers';
+}
+
 // Update The Mobile Navigation
 function updateMobileNav() {
-    let navLink = document.querySelector('.nav-link-projects');
+	let navLink = document.querySelector('.nav-link-projects');
 
-    if (mediaQuery.matches) {
-        navLink.innerHTML = 'Projecten';
-    } else {
-        navLink.innerHTML = 'Maatschappelijke Projecten';
-    }
+	if (mediaQuery.matches) {
+		navLink.innerHTML = 'Projecten';
+	} else {
+		navLink.innerHTML = 'Steun Ons';
+	}
 }
 
 // Create Media Query And Add Event Listener
@@ -16,5 +24,5 @@ window.addEventListener('resize', updateMobileNav);
 
 // Add Event Listener To Logo
 document.querySelector('.logo img').addEventListener('click', function () {
-    window.location.href = '/index.html';
+	window.location.href = '/index.html';
 });
