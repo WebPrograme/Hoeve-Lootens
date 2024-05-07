6ba7214ac14610eb9e7a5ce90a687c1a7e2f26d0
Yarne Gooris <yarne.gooris@gmail.com>
2024-05-07 22:12:24 +0200

    chore: Update button text and style in shop.js and volunteers.js

--- js/maatschappelijk.js
+++ js/maatschappelijk.js
@@ -1,7 +1,7 @@
 import { postRequest } from '../modules/Requests.js';
 import analytics from './analyse.js';
 
-document.querySelector('.maatschappelijk-form .btn').addEventListener('click', async (e) => {
+document.querySelector('.maatschappelijk-form #submit').addEventListener('click', async (e) => {
 	const name = document.querySelector('.maatschappelijk-form #name').value;
 	const email = document.querySelector('.maatschappelijk-form #email').value;
 	const phone = document.querySelector('.maatschappelijk-form #phone').value;
